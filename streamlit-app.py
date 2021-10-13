94a865c449ae75b347cd024f8220731477e1e090
Menno Liefstingh <menno.liefstingh@vantage-ai.com>
2021-10-13 13:12:45 +0200

    Added multiselect to pick squirrel colors to display

--- streamlit-app.py
+++ streamlit-app.py
@@ -7,27 +7,15 @@ import numpy as np
 data = pd.read_csv("https://data.cityofnewyork.us/api/views/vfnx-vebw/rows.csv")
 data = data.rename(columns={"X": "lon", "Y": "lat"})
 data = data.fillna("None")
-print(data["Primary Fur Color"].value_counts())
 
 st.title("Squirrels! 🐿")
+st.markdown("This app showcases a couple functionalities of Streamlit")
 
 # st.write(data)
 
 # st.text("Hello world :)")
 
-map_args = {
-    "pickable": True,
-    "opacity": 1,
-    "stroked": False,
-    "filled": True,
-    "radius_scale": 3,
-    "radius_min_pixels": 2,
-    "radius_max_pixels": 100,
-    "get_position": ["lon", "lat"],
-    "get_radius": 1,
-}
-
-
+# Function that constructs a PyDeck scatter layer to display squirrels
 def squirrel_layer(data, color):
     return pdk.Layer(
         "ScatterplotLayer",
@@ -35,24 +23,36 @@ def squirrel_layer(data, color):
         get_fill_color=color,
         pickable=True,
         opacity=1,
-        stroked=False,
+        stroked=True,
         filled=True,
         radius_scale=3,
-        radius_min_pixels=2,
+        radius_min_pixels=5,
         radius_max_pixels=100,
         get_position=["lon", "lat"],
         get_radius=1,
     )
 
 
-gray_layer = squirrel_layer(
-    data.loc[data["Primary Fur Color"] == "Gray"], [130, 130, 130]
-)
-cinnamon_layer = squirrel_layer(
-    data.loc[data["Primary Fur Color"] == "Cinnamon"], [107, 34, 35]
-)
-black_layer = squirrel_layer(data.loc[data["Primary Fur Color"] == "Black"], [0, 0, 0])
+# Define three different layers for the three different colors
+map_layers = {
+    "Gray": squirrel_layer(
+        data.loc[data["Primary Fur Color"] == "Gray"], [130, 130, 130]
+    ),
+    "Cinnamon": squirrel_layer(
+        data.loc[data["Primary Fur Color"] == "Cinnamon"], [97, 54, 19]
+    ),
+    "Black": squirrel_layer(data.loc[data["Primary Fur Color"] == "Black"], [0, 0, 0]),
+}
 
+layers = st.multiselect(
+    "Fur colors to show on the map:",
+    ["Gray", "Cinnamon", "Black"],
+    ["Gray", "Cinnamon", "Black"],
+)
+# Replace the strings returned by the multiselectbox by the objects mapped in the dict
+layers = [map_layers[color] for color in layers]
+
+# Initialize a PyDeck map
 map = pdk.Deck(
     map_style="road",
     initial_view_state={
@@ -61,10 +61,11 @@ map = pdk.Deck(
         "zoom": 13,
         "pitch": 0,
     },
-    layers=[gray_layer, cinnamon_layer, black_layer],
+    layers=layers,
     tooltip={
-        "text": "Squirrel ID: {Unique Squirrel ID}\n Specific location:{Specific Location}\n Other activities:{Other Activities}"
+        "text": "Squirrel ID: {Unique Squirrel ID}\n Specific location: {Specific Location}\n Other activities: {Other Activities}"
     },
 )
 
+# Render the map as html instead of using st.deck() to circumvent a few Pydeck rendering issues
 st.components.v1.html(map.to_html(as_string=True), height=500)
